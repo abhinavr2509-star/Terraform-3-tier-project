db1b9cf79cb3ce0f39717460ee7d694abff2aaee
abhinavr2509-star <abhinav.r2509@gmail.com>
2026-04-06 21:38:56 +0530

    Update appasg.tf

--- appasg.tf
+++ appasg.tf
@@ -22,7 +22,7 @@ resource "aws_launch_template" "swiggy-app-template" {
   name_prefix   = "swiggy-app-template"
   image_id      = "ami-0ec10929233384c7f"
   instance_type = "t3.micro"
-  key_name      = "raham-m"
+  key_name      = "530PMBATCH"
 
   network_interfaces {
     security_groups             = [aws_security_group.swiggy-ec2-asg-sg-app.id]
