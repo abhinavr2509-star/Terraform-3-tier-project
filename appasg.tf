2319fd6a36a84258aa100ffaf34ad2c9343e2a00
abhinavr2509-star <abhinav.r2509@gmail.com>
2026-04-08 21:50:37 +0530

    Update appasg.tf

--- appasg.tf
+++ appasg.tf
@@ -21,7 +21,7 @@ resource "aws_autoscaling_group" "swiggy-app-asg" {
 resource "aws_launch_template" "swiggy-app-template" {
   name_prefix   = "swiggy-app-template"
   image_id      = "ami-0ec10929233384c7f"
-  instance_type = "t3.micro"
+  instance_type = "t3.small"
   key_name      = "530PMBATCH"
 
   network_interfaces {
