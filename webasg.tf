9f482d59fc80349068d4910bae0a24d08dabfeed
abhinavr2509-star <abhinav.r2509@gmail.com>
2026-04-08 21:53:10 +0530

    Update webasg.tf

--- webasg.tf
+++ webasg.tf
@@ -15,7 +15,7 @@ resource "aws_autoscaling_group" "swiggy-web-asg" {
 resource "aws_launch_template" "swiggy-web-template" {
   name_prefix   = "swiggy-web-template"
   image_id      = "ami-0ec10929233384c7f"
-  instance_type = "t3.micro"
+  instance_type = "t3.small"
   key_name      = "530PMBATCH"
   network_interfaces {
     associate_public_ip_address = true
