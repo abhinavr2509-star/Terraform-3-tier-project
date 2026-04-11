a979ed234d5a9fd30e9559459c9f937207b492c2
abhinavr2509-star <abhinav.r2509@gmail.com>
2026-04-11 19:56:52 +0530

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -3,5 +3,5 @@ yum install httpd git -y
 systemctl start httpd
 systemctl status httpd
 cd /var/www/html
-git clone https://github.com/karishma1521success/swiggy-clone.git
+git clone https://github.com/abhinavr2509-star/swiggy-clone.git
 mv swiggy-clone/* .
