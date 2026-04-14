de00855f20c33304f98440252017e91dec948c19
abhinavr2509-star <abhinav.r2509@gmail.com>
2026-04-14 12:21:02 +0530

    Update appasg.tf

--- appasg.tf
+++ appasg.tf
@@ -20,7 +20,7 @@ resource "aws_autoscaling_group" "swiggy-app-asg" {
 # Create a launch template for the EC2 instances
 resource "aws_launch_template" "swiggy-app-template" {
   name_prefix   = "swiggy-app-template"
-  image_id      = "ami-05d2d839d4f73aafb"
+  image_id      = "ami-0e12ffc2dd465f6e4"
   instance_type = "t3.small"
   key_name      = "ALL.pem"
 
