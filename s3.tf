bf505ea89d8b0e388c996659a5d619b863cf0bac
abhinavr2509-star <abhinav.r2509@gmail.com>
2026-04-14 09:25:45 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,9 +1,11 @@
-# Create an S3 Bucket
 resource "aws_s3_bucket" "example_bucket" {
-  bucket = "swiggy-bucket-terraform"
+  bucket = "swiggy-bucket-terraform-abhinav"
+}
 
-  # Enable versioning
-  versioning {
-    enabled = true
+resource "aws_s3_bucket_versioning" "example" {
+  bucket = aws_s3_bucket.example_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
   }
 }
